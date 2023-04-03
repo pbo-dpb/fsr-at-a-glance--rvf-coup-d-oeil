e2e46f3ef3a528ceca27ca05122518cc912e1432
root <remy.vanherweghem@parl.gc.ca>
2023-04-03 12:21:22 +0000

    Updated sample text.

--- src/components/ProvinceViewNetDebt.vue
+++ src/components/ProvinceViewNetDebt.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script >
-import { defineComponent, ref  } from 'vue';
+import { defineComponent, ref } from 'vue';
 import { BarChart } from 'vue-chart-3';
 import { Chart, registerables } from "chart.js";
 import Province from '../Models/Province';
@@ -13,55 +13,55 @@ Chart.register(...registerables);
 export default defineComponent({
   components: { BarChart },
   props: {
-      province: {type: Province, required: true},
+    province: { type: Province, required: true },
   },
-  
-  computed: {
-      options() {
-return {
-      responsive: true,
-      aspectRatio: 3,
-      plugins: {
-        legend: {
-          display:false
-        },
-        tooltip: {
-                callbacks: {
-                    label: (context) => {
-                        let label = context.dataset.label || '';
 
-                        if (label) {
-                            label += ': ';
-                        }
-                        if (context.parsed.y !== null) {
-                            label += this.$root.strings.__('percentage_of_tooltip_label', {percentage: new Intl.NumberFormat(`${this.$root.strings.language}-CA`, { style: 'percent' }).format(context.parsed.y/100)});
-                        }
-                        return label;
-                    }
+  computed: {
+    options() {
+      return {
+        responsive: true,
+        aspectRatio: 3,
+        plugins: {
+          legend: {
+            display: false
+          },
+          tooltip: {
+            callbacks: {
+              label: (context) => {
+                let label = context.dataset.label || '';
+
+                if (label) {
+                  label += ': ';
                 }
-        }
-      },
-      scales: {
-        y: {
-        display: true,
-        title: {
-          display: true,
-          text: this.$root.strings.percentage_of_gdp_axis_label,
+                if (context.parsed.y !== null) {
+                  label += this.$root.strings.__('percentage_of_tooltip_label', { percentage: new Intl.NumberFormat(`${this.$root.strings.language}-CA`, { style: 'percent' }).format(context.parsed.y / 100) });
+                }
+                return label;
+              }
+            }
+          }
+        },
+        scales: {
+          y: {
+            display: true,
+            title: {
+              display: true,
+              text: this.$root.strings.percentage_of_gdp_axis_label,
+            }
+          }
         }
       }
-      }
-    }
-      },
-      backgroundColors() {
-          return this.$root.payload.labels.map(label=>{
-              return label <= this.$root.payload.year ? '#0072AF' : '#19AEFF';
-          });
-      },
-      chartData () {
-          return {
+    },
+    backgroundColors() {
+      return this.$root.payload.labels.map(label => {
+        return label <= this.$root.payload.year ? '#002140' : '#a2b8e1';
+      });
+    },
+    chartData() {
+      return {
         labels: this.$root.payload.labels,
         datasets: [
-         {
+          {
             data: this.province.net_debt.values,
             backgroundColor: this.backgroundColors,
           },
--- src/components/ProvinceViewProgramSpendingRevenue.vue
+++ src/components/ProvinceViewProgramSpendingRevenue.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script >
-import { defineComponent, ref  } from 'vue';
+import { defineComponent, ref } from 'vue';
 import { LineChart } from 'vue-chart-3';
 import { Chart, registerables } from "chart.js";
 import Province from '../Models/Province';
@@ -13,77 +13,77 @@ Chart.register(...registerables);
 export default defineComponent({
   components: { LineChart },
   props: {
-      province: {type: Province, required: true},
+    province: { type: Province, required: true },
   },
   computed: {
-      options() {
-return {
-      responsive: true,
-      aspectRatio: 3,
-       interaction: {
-    intersect: false
-  },
-      radius: 0,
-      plugins: {
-        legend: {
-          display:true,
-          position:"bottom"
+    options() {
+      return {
+        responsive: true,
+        aspectRatio: 3,
+        interaction: {
+          intersect: false
         },
-        tooltip: {
-                callbacks: {
-                    label: (context) => {
-                        let label = context.dataset.label || '';
+        radius: 0,
+        plugins: {
+          legend: {
+            display: true,
+            position: "bottom"
+          },
+          tooltip: {
+            callbacks: {
+              label: (context) => {
+                let label = context.dataset.label || '';
 
-                        if (label) {
-                            label += ': ';
-                        }
-                        if (context.parsed.y !== null) {
-                            label += this.$root.strings.__('percentage_of_tooltip_label', {percentage: new Intl.NumberFormat(`${this.$root.strings.language}-CA`, { style: 'percent' }).format(context.parsed.y/100)});
-                        }
-                        return label;
-                    }
+                if (label) {
+                  label += ': ';
                 }
-        }
-      },
-      scales: {
-        y: {
-        display: true,
-        title: {
-          display: true,
-          text: this.$root.strings.percentage_of_gdp_axis_label,
+                if (context.parsed.y !== null) {
+                  label += this.$root.strings.__('percentage_of_tooltip_label', { percentage: new Intl.NumberFormat(`${this.$root.strings.language}-CA`, { style: 'percent' }).format(context.parsed.y / 100) });
+                }
+                return label;
+              }
+            }
+          }
+        },
+        scales: {
+          y: {
+            display: true,
+            title: {
+              display: true,
+              text: this.$root.strings.percentage_of_gdp_axis_label,
+            }
+          }
         }
       }
-      }
-    }
-      },
-      
-      chartData () {
-          return {
+    },
+
+    chartData() {
+      return {
         labels: this.$root.payload.labels,
         datasets: [
-         {
+          {
             data: this.province.revenue.values,
             label: this.$root.strings.revenue_label,
-            borderColor: "#0072AF",
+            borderColor: "#2d5071",
             cubicInterpolationMode: 'monotone',
             segment: {
-            borderDash: ctx => {
-              if (ctx.p0.parsed.x > this.$root.payload.labels.indexOf(this.$root.payload.year)) return [6,6];
-              return undefined; 
+              borderDash: ctx => {
+                if (ctx.p0.parsed.x > this.$root.payload.labels.indexOf(this.$root.payload.year)) return [6, 6];
+                return undefined;
+              },
             },
-        },
           },
           {
             data: this.province.program_spending.values,
             label: this.$root.strings.program_spending_label,
-            borderColor: "#AD3D00",
+            borderColor: "#cd5e76",
             cubicInterpolationMode: 'monotone',
             segment: {
-            borderDash: ctx => {
-              if (ctx.p0.parsed.x > this.$root.payload.labels.indexOf(this.$root.payload.year)) return [6,6];
-              return undefined; 
+              borderDash: ctx => {
+                if (ctx.p0.parsed.x > this.$root.payload.labels.indexOf(this.$root.payload.year)) return [6, 6];
+                return undefined;
+              },
             },
-        },
           },
         ],
       }
