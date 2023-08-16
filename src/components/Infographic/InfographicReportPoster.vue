8961f11cc1ebd052aa070475b616d63ec3f47d4f
Rémy Vanherweghem <remy.vanherweghem@parl.gc.ca>
2023-08-16 18:53:37 +0000

    Removed red background in infographic.

--- src/components/Infographic/InfographicReportPoster.vue
+++ src/components/Infographic/InfographicReportPoster.vue
@@ -1,13 +1,15 @@
 <template>
-    <div class="flex flex-col items-center p-4 gap-2 w-full  border-2 border-blue-100 shadow-inner rounded text-center">
-        <img :src="selectedYear.fsr_poster_url?.[language]" alt="" aria-hidden="true"
-            class="h-32 shadow border border-blue-100" loading="lazy">
+    <div class="flex flex-col items-center p-4 gap-2 w-full  text-center">
+        <a :href="selectedYear.fsr_url[language]"><img :src="selectedYear.fsr_poster_url?.[language]" alt=""
+                aria-hidden="true" class="h-32 shadow border border-blue-100" loading="lazy">
+        </a>
         <p class="font-semibold leading-snug">
             {{ strings.report_poster_blurb }}
         </p>
 
-        <a :href="selectedYear.fsr_url" class="bg-white rounded hover:bg-blue-100 dark:bg-blue-900 font-semibold p-2">{{
-            strings.report_poster_cta }}</a>
+        <a :href="selectedYear.fsr_url[language]"
+            class="bg-white rounded hover:bg-blue-100 dark:bg-blue-900 font-semibold p-2">{{
+                strings.report_poster_cta }}</a>
 
     </div>
 </template>
